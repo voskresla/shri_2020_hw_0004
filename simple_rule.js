4023de9fb535097deacc522b13af01f56948a6ce
Stepan Voskresla <filthy.and.filthy@gmail.com>
2020-03-21 17:37:20 +0300

    mvp done

--- simple_rule.js
+++ simple_rule.js
@@ -1,17 +1,15 @@
-import property from "./lodash-es/property";
-import flatMap from "./lodash-es/flatMap";
-
-window._ = "aaa";
+import property from './lodash-es/property';
+import flatMap from './lodash-es/flatMap';
+import map from './lodash-es/map';
 
 function getScope(scopeManager, currentNode) {
-  // On Program node, get the outermost scope to avoid return Node.js special function scope or ES modules scope.
-  const inner = currentNode.type !== "Program";
+  const inner = currentNode.type !== 'Program';
 
   for (let node = currentNode; node; node = node.parent) {
     const scope = scopeManager.acquire(node, inner);
 
     if (scope) {
-      if (scope.type === "function-expression-name") {
+      if (scope.type === 'function-expression-name') {
         return scope.childScopes[0];
       }
       return scope;
@@ -23,28 +21,23 @@ function getScope(scopeManager, currentNode) {
 
 function collectParameterValues(node) {
   switch (node && node.type) {
-    case "Identifier":
+    case 'Identifier':
       return [node.name];
-    case "ObjectPattern":
-      return flatMap(node.properties, prop =>
-        collectParameterValues(prop.value)
-      );
-    case "ArrayPattern":
+    case 'ObjectPattern':
+      return flatMap(node.properties, prop => collectParameterValues(prop.value));
+    case 'ArrayPattern':
       return flatMap(node.elements, collectParameterValues);
     default:
       return [];
   }
 }
 
-const getCaller = property(["callee", "object"]);
-const getMethodName = property(["callee", "property", "name"]);
+const getCaller = property(['callee', 'object']);
+const getMethodName = property(['callee', 'property', 'name']);
 
-const isArrayExpression = node =>
-  node.type === "ArrayExpression" ? true : false;
-const isObjectExpression = node =>
-  node.type === "ObjectsExpression" ? true : false;
-const isInReturn = node =>
-  node.parent.type === "ReturnStatement" ? true : false;
+const isArrayExpression = node => (node.type === 'ArrayExpression' ? true : false);
+const isObjectExpression = node => (node.type === 'ObjectsExpression' ? true : false);
+const isInReturn = node => (node.parent.type === 'ReturnStatement' ? true : false);
 // TODO: сделать функцию которая ищем в скоупах и если не находить то false
 const variableIsArray = node => false;
 const isLodashMap = node => {
@@ -54,44 +47,83 @@ const isLodashMap = node => {
   // gпосомтри правило no-use-before-define
 
   // проверить что его никто не переопределил
-  return getCaller(node).name === "_" && getMethodName(node) === "map";
+  return getCaller(node).name === '_' && getMethodName(node) === 'map';
 };
 
+// Проверяем что проверка на массив:
+// - нужного аргумента
+// - через Array.isArray()
+// - TODO: придумать все возможные проверки на массив и проверять их тоже.
+//   Например typeof.
 const isNativeArrayIsArrayCheck = (node, test, context) => {
-  // console.log("inNative");
-  // console.log("test arg", test.arguments[0]);
-  // console.log("node arg", node.arguments[0]);
-  // console.log(context.getSourceCode().scopeManager);
-  if (getCaller(test).name !== "Array") return false;
-  if (getMethodName(test) !== "isArray") return false;
+  if (getCaller(test).name !== 'Array') return false;
+  if (getMethodName(test) !== 'isArray') return false;
   if (test.arguments.length !== 1) return false;
   if (test.arguments[0].name !== node.arguments[0].name) return false;
 
   return true;
 };
-// проверяем что:
-// - _.map лежит в тернарке
-// - _.map лежит в alternate
-// - test - проверяет первый аргумент из map() на массив с помощью Array.isArray(arg1)
-// - consequent - вызывает нативный метод Array#map на arg1
 
-// но тогда надо проверять инверсию условий? Если в test лежит !Array
-const isMemberOfCorrectTernaryOfArrayCheking = (node, context) => {
-  console.log(node);
-  if (node.parent.type !== "ConditionalExpression") return false;
-  if (node.parent.alternate !== node) return false;
-  if (!isNativeArrayIsArrayCheck(node, node.parent.test, context)) return false;
-  console.log("fire god ternary");
+// Проверяем, что:
+// _.map лежит в IfStatment
+// _.map лежит в Alternate
+// - IfStatement:Test проверяет первый аргумент из map() на массив с
+//   помощью Array.isArray(arg1), без инверсии
+// - IfStatemnet:Test вызывает нативный метод Array#map на arg1
+const inIfStatementAlternate = mapNode => {
+  // поднимаемся по родителям пока parent не станет равным ifStatement у
+  // которого в фдеуктфеу текущая нода ?
+  let isMapInIf = false;
+  let tmpNode = mapNode;
+  let ifStatementTest = null;
+
+  while (tmpNode.parent && !isMapInIf) {
+    if (
+      tmpNode.parent &&
+      tmpNode.parent.type === 'IfStatement' &&
+      tmpNode.parent.alternate === tmpNode
+    ) {
+      isMapInIf = true;
+      ifStatementTest = tmpNode.parent;
+    } else {
+      tmpNode = tmpNode.parent;
+    }
+  }
+
+  if ((isMapInIf && !isNativeArrayIsArrayCheck(mapNode, ifStatementTest.test)) || !isMapInIf)
+    return false;
+
+  return true;
+};
+
+// Проверяем, что:
+// _.map лежит в ConditionalExpression
+// _.map лежит в Alternate
+// - ConditionalExpression:Test проверяет первый аргумент из map() на массив с
+//   помощью Array.isArray(arg1), без инверсии
+// - ConditionalExpression:Test вызывает нативный метод Array#map на arg1
+const inConditionalExpressionAlternate = mapNode => {
+  if (mapNode.parent.type !== 'ConditionalExpression') return false;
+  if (mapNode.parent.alternate !== mapNode) return false;
+  if (!isNativeArrayIsArrayCheck(mapNode, mapNode.parent.test)) return false;
+
   return true;
 };
 
 const isAppropriateMapMethod = (node, context) => {
-  if (getCaller(node).name !== "_") return false;
-  if (getMethodName(node) !== "map") return false;
+  if (getCaller(node).name !== '_') return false;
+  if (getMethodName(node) !== 'map') return false;
   if (node.arguments.length !== 2) return false;
-  if (isArrayExpression(node.arguments[0])) return false;
-  if (isMemberOfCorrectTernaryOfArrayCheking(node, context)) return false;
-  console.log("fire apropriate method");
+
+  // Нужно ли вобще фиксить
+
+  // Если _.map в IfStatement:alternate
+  // NOTE: пока проверка без возможной инверсии alternate / consequent
+  if (inIfStatementAlternate(node)) return false;
+
+  // Есди _.map в ConditionalStatement:alternate
+  // NOTE: пока проверка без возможной инверсии alternate / consequent
+  if (inConditionalExpressionAlternate(node)) return false;
   return true;
 };
 
@@ -100,141 +132,53 @@ export default function(context) {
   const globalLodashReference = null;
 
   return {
-    // 'Program'(node) {
-    //   const scopeManager = context.getSourceCode().scopeManager
-    //   // console.log(getScope(scopeManager, node))
-    //   console.log(scopeManager)
-    //   console.log(scopeManager.globalScope.variables.filter(e => e.name === '_'))
-    //   console.log(context.getDeclaredVariables())
-    // },
-
     CallExpression(node) {
-      // не работает с чейнингомs
+      console.log(node);
+      // не работает с чейнингом
       // нашли CallExpression _.map подходящий под возможность замены
       if (isAppropriateMapMethod(node, context)) {
-        console.log("fire map is apropriate for change");
-        const arg1 = node.arguments[0];
-        const arg2 = node.arguments[1];
-        let fullString = "";
-        let currentNode = context.getSourceCode().getText(node);
-        let returnString = "";
+        const mapCallExpString = context.getSourceCode().getText(node);
 
-        if (isInReturn(node)) {
-          returnString = "return";
-        }
+        const mapCallExpArg1 = node.arguments[0];
+        const mapCallExpArg2 = node.arguments[1];
 
-        if (variableIsArray(arg1)) {
-          // проверка не нужна
-        } else {
-          // дополняем строку
-          fullString = `Array.isArray(${arg1.name}) ?
-                ${returnString} ${arg1.name}.map(${arg2.name})
-              :
-                ${returnString} ${currentNode}
-              
-              `;
-        }
+        let finalFixString = '';
+        let returnWord = '';
+        let arrayCheckString = '';
+
+        // Нужна ли проверка на массив
+        if (!isArrayExpression(node.arguments[0]))
+          arrayCheckString = `if (Array.isArray(${mapCallExpArg1}))`;
+
+        // Нужен ли return
+        if (isInReturn(node)) returnWord = 'return';
+
+        let finalCheckString = `
+          ${
+            !isArrayExpression(mapCallExpArg1)
+              ? `if (Array.isArray(${mapCallExpArg1.name})) {`
+              : null
+          }
+          ${returnWord} ${mapCallExpArg1.name}.map(${context
+          .getSourceCode()
+          .getText(mapCallExpArg2)})
+          ${!isArrayExpression(mapCallExpArg1) ? `}` : null}
+          else {
+            ${returnWord} ${mapCallExpString}
+          }
+        `;
+
+        console.log(finalCheckString);
 
         context.report({
           node,
-          message: "blabla",
+          message: 'Замена',
           fix(fixier) {
             console.log();
-            return fixier.replaceText(
-              isInReturn(node) ? node.parent : node,
-              fullString
-            );
-          }
+            return fixier.replaceText(isInReturn(node) ? node.parent : node, finalCheckString);
+          },
         });
       }
-      // if (
-      //   true &&
-      //   getCaller(node).name === "_" &&
-      //   getMethodName(node) === "map" &&
-      //   node.arguments.length === 2 &&
-      //   !isArrayExpression(node.arguments[0])
-
-      //   // && !isObjectExpression(node.arguments[0])
-      // ) {
-      //   const scopeManager = context.getSourceCode().scopeManager;
-      //   console.log(getScope(scopeManager, node));
-      //   console.log("arg1:", node.arguments[0]);
-      //   console.log("arg2:", node.arguments[1]);
-      //   // isMemberOfTernary(node)
-      //   const arg1 = node.arguments[0];
-      //   const arg2 = node.arguments[1];
-      //   // формируем замену
-      //   let fullString = "";
-      //   let currentNode = context.getSourceCode().getText(node);
-      //   let returnString = "";
-
-      //   if (isInReturn(node)) {
-      //     returnString = "return";
-      //   }
-
-      //   if (variableIsArray(arg1)) {
-      //     // проверка не нужна
-      //   } else {
-      //     // дополняем строку
-      //     fullString = `if (Array.isArray(${arg1.name})) {
-      //           ${returnString} ${arg1.name}.map(${arg2.name})
-      //         } else {
-      //           ${returnString} ${currentNode}
-      //         }
-      //         `;
-      //   }
-
-      //   context.report({
-      //     node,
-      //     message: "blabla",
-      //     fix(fixier) {
-      //       console.log();
-      //       return fixier.replaceText(
-      //         isInReturn(node) ? node.parent : node,
-      //         fullString
-      //       );
-      //     }
-      //   });
-      // }
-
-      // if (getCaller(node).name == '_') {
-      //   console.log(node)
-      // }
-      // нашел кто зовет метод
-      // console.log(getCaller(node).name)
-      // console.log(node.arguments)
-
-      // console.log(collectParameterValues(node.arguments[0]))
-      // нашел название метода
-      // console.log(getMethodName(node))
-
-      // проверить аргументы метода: что их 2, что первый не объект, не arrayDedclaration, определен в scope, если не определен то похуй
-
-      const sourceCode = context.getSourceCode();
-
-      // if (node.name && node.name == "map") {
-      //   const xxx = getScope(sourceCode.scopeManager, node);
-      //   console.log("get scope", xxx);
-      //   console.log(node);
-      //   //console.log('var',context.getDeclaredVariables(node))
-      //   const firstArgument = node.parent.parent.arguments[0];
-      //   //console.log(sourceCode.scopeManager.acquire(firstArgument));
-      //   console.log(`collection scope:`, getScope(sourceCode.scopeManager, firstArgument));
-      //   const secondArgument = node.parent.parent.arguments[1];
-
-      //   const startPosition = node.parent.object.start; // 36
-      //   const lastPosition = node.parent.parent.end; // 86
-
-      //   const txt = `if () {return ${firstArgument.name}.map(${sourceCode.getText(secondArgument)}) }`;
-      //   context.report({
-      //     node,
-      //     message: "blabla",
-      //     fix(fixier) {
-      //       //console.log(startPosition)
-      //       //return fixier.replaceTextRange([startPosition, lastPosition], txt)
-      //     }
-      //   });
-      // }
-    }
+    },
   };
 }
