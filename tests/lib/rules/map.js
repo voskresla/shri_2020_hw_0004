9d03f4c3620e7dafeecb4fbdcb49c5b1ed8625a0
Stepan Voskresla <filthy.and.filthy@gmail.com>
2020-03-22 22:20:44 +0300

    fix return nothing if no fix

--- tests/lib/rules/map.js
+++ tests/lib/rules/map.js
@@ -15,9 +15,14 @@ const rule = require('../../../lib/rules/map'),
 // Tests
 //------------------------------------------------------------------------------
 
-var ruleTester = new RuleTester();
-ruleTester.run('map', rule, {
-  valid: ['[].map(fn)'],
+var ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 6 } });
+ruleTester.run('Simple test', rule, {
+  valid: [
+    '[].map(fn)',
+    '_.map("string",fn)',
+    'Array.isArray(x)?x.map(fn):_.map(x,fn)',
+    'if (Array.isArray(x)) { x.map(fn) } else { _.map(x,fn) } ',
+  ],
 
   invalid: [
     {
@@ -26,6 +31,48 @@ ruleTester.run('map', rule, {
       errors: [
         {
           message: 'Будем фиксить c проверкой, потому что неизвестно что в аргументе:',
+          type: 'CallExpression',
+        },
+      ],
+    },
+    {
+      code: '_.map([], fn)',
+      output: `[].map(fn)`,
+      errors: [
+        {
+          message: 'Будем фиксить без проверки, потому что аргумент Array:',
+          type: 'CallExpression',
+        },
+      ],
+    },
+  ],
+});
+
+ruleTester.run('Reassignment && Redeclaration', rule, {
+  valid: [
+    '_ = 0; _.map("string",fn)',
+    'let _ = 0; _.map("string",fn)',
+    'let a = []; { a = 0; _.map(a,fn) }',
+  ],
+
+  invalid: [
+    {
+      code: 'let a = []; { _.map(a,fn) }',
+      output: `let a = []; { a.map(fn) }`,
+      errors: [
+        {
+          message: 'Будем фиксить без проверки, потому что аргумент Array:',
+          type: 'CallExpression',
+        },
+      ],
+    },
+    {
+      code: 'let a = []; { a = 0; _.map(a, fn) }; _.map(a,fn2)',
+      parserOptions: { ecmaVersion: 6 },
+      output: `options`,
+      errors: [
+        {
+          message: 'Будем фиксить без проверки, потому что аргумент Array:',
           type: 'CallExpression',
         },
       ],
